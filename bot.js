daabba7ce31f0a3b477888b086187ecd9dcae30b
jwile14 <jwile14@gmail.com>
2019-01-26 13:45:14 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,57 @@
+var HTTPS = require('https');
+var cool = require('cool-ascii-faces');
+
+var botID = process.env.BOT_ID;
+
+function respond() {
+    const request = JSON.parse(this.req.chunks[0]),
+        botRegex = /^\/TK/;
+
+    if (request.text && botRegex.test(request.text)) {
+        this.res.writeHead(200);
+        postMessage();
+        this.res.end();
+    } else {
+        console.log("don't care");
+        this.res.writeHead(200);
+        this.res.end();
+    }
+}
+
+function postMessage() {
+    const botResponse = cool();
+
+    const options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+    };
+
+    const body = {
+        "bot_id": botID,
+        "text": botResponse
+    };
+
+    console.log('sending ' + botResponse + ' to ' + botID);
+
+    const botReq = HTTPS.request(options, function(res) {
+        if (res.statusCode == 202) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
+
+    botReq.on('error', function(err) {
+        console.log('error posting message ' + JSON.stringify(err));
+    });
+    botReq.on('timeout', function(err) {
+        console.log('timeout posting message ' + JSON.stringify(err));
+    });
+    botReq.end(JSON.stringify(body));
+}
+
+
+module.exports = {
+    respond
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const director = require('director');
+const cool = require('cool-ascii-faces');
+const bot = require('./bot.js');
+
+const frouter = new director.http.Router({
+    '/': {
+        post: bot.respond,
+        get: ping
+    }
+});
+
+const server = http.createServer(function(req, res) {
+    req.chunks = [];
+    req.on('data', function(chunk) {
+        req.chunks.push(chunk.toString());
+    });
+
+    router.dispatch(req, res, function(err) {
+        res.writeHead(err.status, {
+            "Content-Type": "text/plain"
+        });
+        res.end(err.message);
+    });
+});
+
+const port = Number(process.env.PORT || 5000);
+server.listen(port);
+
+function ping() {
+    this.res.writeHead(200);
+    this.res.end("Hey, I'm Cool Guy.");
+}
