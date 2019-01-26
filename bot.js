8d96504193b784a57fe6875978d2b91f335016ef
jwile14 <jwile14@gmail.com>
2019-01-26 14:52:50 -0500

    Add multiple bot ID handling

--- bot.js
+++ bot.js
@@ -2,16 +2,24 @@ const HTTPS = require('https');
 const cool = require('cool-ascii-faces');
 const sleep = require('sleep');
 
-const botID = process.env.BOT_ID;
+const BOT_ID = process.env.BOT_ID;
+const TEST_BOT_ID = process.env.TEST_BOT_ID;
 
 function respond() {
     const request = JSON.parse(this.req.chunks[0]);
-    const botRegex = /^Hey Farkas,/;
+    const botRegex = /^\/molander/;
+    const testRegex = /^\/test/;
 
     if (request.text && botRegex.test(request.text)) {
-        this.res.writeHead(200);
-        postMessage();
-        this.res.end();
+        if (botRegex.test(request.text)) {
+            this.res.writeHead(200);
+            postMessage(BOT_ID);
+            this.res.end();
+        } else if (testRegex.test(request.text)) {
+            this.res.writeHead(200);
+            postMessage(TEST_BOT_ID);
+            this.res.end();
+        }
     } else {
         console.log("Message ignored");
         this.res.writeHead(200);
@@ -19,7 +27,7 @@ function respond() {
     }
 }
 
-function postMessage() {
+function postMessage(botID) {
     const botResponse = cool();
 
     const options = {
@@ -39,7 +47,7 @@ function postMessage() {
 
     const botReq = HTTPS.request(options, function(res) {
         if (res.statusCode == 202) {
-            //neat
+            // neat
         } else {
             console.log('rejecting bad status code ' + res.statusCode);
         }
