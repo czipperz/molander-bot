e9a75ed33d360114d0aaaaa74a13e9ad6c34c5ec
jwile14 <jwile14@gmail.com>
2019-01-27 11:24:17 -0500

    Remove unused files

--- test.js
+++
@@ -1,6 +0,0 @@
-const bot = require('./bot');
-
-bot.respondTo("/molander-help", function (id, message) { console.log("Error: /molander-help"); });
-bot.respondTo("/molander", function (id, message) { console.log(id + " says '" + message + "'"); });
-bot.respondTo("/molander help", function (id, message) { console.log(id + " says '" + message + "'"); });
-bot.respondTo("/test help", function (id, message) { console.log(id + " says '" + message + "'"); });
