707f87956793a9e7ae474c6459f88a49cd7e349d
Chris Gregory <czipperz@gmail.com>
2019-01-27 21:37:20 -0500

    Expand deus vult command (#17)

--- src/commands/deus-vult.js
+++ src/commands/deus-vult.js
@@ -1,11 +1,15 @@
-const commandRegex = /infidels.*\!$/;
+const commandRegex = /(infidel|crusade|heretic)/;
 
 function processCommand(command) {
+    const images = [
+        "https://ih0.redbubble.net/image.297633040.5334/flat,550x550,075,f.u2.jpg",
+        "https://i.kym-cdn.com/photos/images/newsfeed/001/176/880/239.jpg",
+    ];
     return {
         attachments: [
             {
                 type: "image",
-                url: "https://ih0.redbubble.net/image.297633040.5334/flat,550x550,075,f.u2.jpg"
+                url: images[Math.floor(Math.random() * images.length)],
             }
         ]
     };
